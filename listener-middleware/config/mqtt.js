255b2f505773a6ca1a309c44ef416222b127c52b
Ivan Ren Villamora <villamora.ivanren.manguiat@gmail.com>
2025-11-26 10:46:47 +0800

    Export config constants

---
+++ listener-middleware/config/mqtt.js
@@ -0,0 +1,7 @@
+export const MQTT_URL = "mqtt://localhost:1883";
+export const MQTT_TOPICS = {
+    TOPIC_LOCATION: "cane/location",
+    TOPIC_DESTINATION: "guardian/destination",
+    TOPIC_INSTRUCTIONS: "cane/instructions",
+    TOPIC_STATUS: "cane/status"
+};
