27de6244a32a00165e9948c566eeabb5347f657e
Ivan Ren Villamora <villamora.ivanren.manguiat@gmail.com>
2026-01-28 19:06:27 +0800

    Add send notes routes

--- listener-middleware/app.js
+++ listener-middleware/app.js
@@ -3,12 +3,24 @@ import express from "express";
 import http from "http";
 import { setupSocketRoutes } from "./services/socketRoutes.js";
 import { initSocket } from "./services/socket.js";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 4000;
+app.use(bodyParser.json());
 
+app.post("/send-note", (req, res) => {
+    const { message } = req.body;
+
+    if (!message || !message.trim()) {
+        return res.status(400).json({ success: false, error: "Message cannot be empty" });
+    }
+
+    return res.json({ success: true, message: "Message sent to ESP32" });
+});
+
+const port = process.env.PORT || 4000;
 const server = http.createServer(app);
 const io = initSocket(server);
 
